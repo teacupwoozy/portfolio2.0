c6e5bb7c35c0850e7656342ac8fdcb510ec93490
Stacy Montemayor <stacy.montemayor@gmail.com>
2019-02-05 16:51:48 -0600

    inital commit

---
+++ config/site.js
@@ -0,0 +1,46 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Stacy Montemayor', // Navigation and Site Title
+  siteTitleAlt: 'Stacy Montemayor - data munging and visualization', // Alternative Site title for SEO
+  siteTitleShort: 'Stacy', // short_name for manifest
+  siteHeadline: 'Storytelling with data munging and visualizations', // Headline for schema.org JSONLD
+  siteUrl: 'https://github.com/teacupwoozy', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo.png', // Used for SEO and manifest
+  siteDescription: 'Portfolio website for Stacy Montemayor',
+  author: 'Stacy Montemayor', // Author for schema.org JSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@TeacupWoozy', // Twitter Username
+  // ogSiteName: 'emilia', // Facebook Site Name
+  ogLanguage: 'en_US', // og:language
+  // googleAnalyticsID: 'UA-12345689-1',
+
+  // Manifest and Progress color
+  themeColor: '#f7fbf7',
+  backgroundColor: '#f7fbf7',
+
+  // Your information
+  avatar: '/logos/lekoarts.png',
+  name: 'Stacy Montemayor',
+  location: 'North America',
+  socialMedia: [
+    {
+      url: 'https://github.com/teacupwoozy',
+      name: 'GitHub',
+    },
+    {
+      url: 'https://twitter.com/teacupwoozy',
+      name: 'Twitter',
+    },
+    {
+      url: 'https://www.linkedin.com/in/stacymontemayor/',
+      name: 'LinkedIn',
+    },
+    {
+      url: 'https://dev.to/stacy',
+      name: 'dev.to',
+    },
+  ],
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Content = styled.footer`
+  color: ${props => props.theme.colors.secondary};
+  text-align: center;
+  font-size: 0.9rem;
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  background: ${props => props.theme.colors.bg};
+`
+
+const Footer = () => (
+  <Content>
+    &copy; 2019 by Stacy Montemayor {' '}
+    <br />
+  </Content>
+)
+
+export default Footer
